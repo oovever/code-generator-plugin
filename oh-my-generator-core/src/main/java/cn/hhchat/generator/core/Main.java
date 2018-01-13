e5874b372f9bebcbc925c3d2cfcbb3fb25df24b9
Codemupf <mupengfei6688@163.com>
2018-01-13 16:36:12 +0800

    Refactor maven project

---
+++ oh-my-generator-core/src/main/java/cn/hhchat/generator/core/Main.java
@@ -0,0 +1,16 @@
+package cn.hhchat.generator.core;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author OovEver
+ * 2018/1/13 16:31
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.error("aaa");
+    }
+}
